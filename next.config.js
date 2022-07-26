b1210e82f1583378f915801fed611b0e8a76e7dc
Shreyas Kishore <i@0xsk.io>
2022-07-25 21:00:28 -0400

    Updated Resume file

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
     return [
       {
         source: '/resume',
-        destination: '/files/Shreyas Kishore Résumé v3.1.3.pdf',
+        destination: '/files/Shreyas Kishore Résumé v3.public.1.pdf',
         permanent: true,
       },
     ];
